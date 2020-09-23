e6e1386c73851ae5a57e200546506d78c88a6c66
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-09-23 17:31:28 -0300

    tdd example - parametirezed test added

--- src/test/java/practicalUnitTesting/chapter4/FootballTeamTest.java
+++ src/test/java/practicalUnitTesting/chapter4/FootballTeamTest.java
@@ -3,14 +3,23 @@ package practicalUnitTesting.chapter4;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
 public class FootballTeamTest {
 
-	@Test
-	public void constructorShouldSetGamesWon() {
-	
-		FootballTeam team = new FootballTeam(3);
-		assertEquals(3, team.getGamesWon());
+	public Object[] nbOfGamesWon() {
+		return new Object[] {0, 1, 2};
 	}
-
+	
+	@Test
+	@Parameters(method = "nbOfGamesWon")
+	public void constructorShouldSetGamesWon(int nbOfGamesWon) {
+		FootballTeam team = new FootballTeam(nbOfGamesWon);
+		assertEquals(nbOfGamesWon + " games passed to constructor, " + "but " + team.getGamesWon() + " were returned",
+				nbOfGamesWon, team.getGamesWon());
+	}
 }
