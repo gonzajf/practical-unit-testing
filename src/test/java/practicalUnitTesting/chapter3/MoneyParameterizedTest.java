2627dd019a3da9491cadd95e1f242c2b914869b6
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-09-22 23:22:13 -0300

    money tests - junit 5 version

--- src/test/java/practicalUnitTesting/chapter3/MoneyParameterizedTest.java
+++ src/test/java/practicalUnitTesting/chapter3/MoneyParameterizedTest.java
@@ -1,25 +1,21 @@
 package practicalUnitTesting.chapter3;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import junitparams.JUnitParamsRunner;
-import junitparams.Parameters;
-
-@RunWith(JUnitParamsRunner.class)
 public class MoneyParameterizedTest {
 
-	private static final Object[] getMoney() {
-		return new Object[] { new Object[] { 10, "USD" }, new Object[] { 20, "EUR" } };
-	}
-
-	@Test
-	@Parameters(method = "getMoney")
+	@ParameterizedTest
+	@MethodSource
 	public void constructorShouldSetAmountAndCurrency(int amount, String currency) {
 		Money money = new Money(amount, currency);
 		assertEquals(amount, money.getAmount());
 		assertEquals(currency, money.getCurrency());
+	}
+
+	private static final Object[] constructorShouldSetAmountAndCurrency() {
+		return new Object[] { new Object[] { 10, "USD" }, new Object[] { 20, "EUR" } };
 	}
 }
