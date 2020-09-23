8acc0831dca2ec5d12dab88eee240cc3703228f7
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-09-23 18:23:30 -0300

    tdd example - comparing two teams test pass

--- src/main/java/practicalUnitTesting/chapter4/FootballTeam.java
+++ src/main/java/practicalUnitTesting/chapter4/FootballTeam.java
@@ -17,8 +17,11 @@ public class FootballTeam implements Comparable<FootballTeam> {
 
 	@Override
 	public int compareTo(FootballTeam otherTeam) {
+		
 		if (gamesWon > otherTeam.getGamesWon()) {
 			return 1;
+		} else if (gamesWon < otherTeam.getGamesWon()) {
+			return -1;
 		}
 		return 0;
 	}
