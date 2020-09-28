926f7ecc0a305dd4643cb7fcecc620ab74d409f5
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-09-28 01:09:15 -0300

    using hamcrest testing collections

---
+++ src/test/java/practicalUnitTesting/chapter6/CollectionTest.java
@@ -0,0 +1,44 @@
+package practicalUnitTesting.chapter6;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.collection.IsMapContaining.hasEntry;
+import static org.hamcrest.collection.IsMapContaining.hasKey;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class CollectionTest {
+
+	@Test
+	public void collectionsUtilityMethods() {
+
+		Set<String> setA = new LinkedHashSet<String>();
+		String s1 = "s1";
+		String s2 = "s2";
+
+		setA.add(s1);
+		setA.add(s2);
+
+		assertThat(setA, hasItem(s1));
+		assertThat(setA, hasItem(s2));
+		assertThat(setA, not(hasItem("xyz")));
+	}
+
+	@Test
+	public void mapsUtilityMethods() {
+		
+		HashMap<String, Integer> map = new LinkedHashMap<String, Integer>();
+		
+		map.put("a", 2);
+		map.put("b", 3);
+		
+		assertThat(map, hasEntry("a", 2));
+		assertThat(map, hasKey("b"));
+	}
+}
